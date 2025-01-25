8d60b84b97fe72d69c0eeb42eb7d65b52e4669d5
Katerina-99 <rakitinakaty555@gmail.com>
2025-01-25 15:28:52 +0000

    created functions for add and check tasks

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,1 +1,21 @@
+const taskInput = document.getElementById("input");
+const addButton = document.getElementById("add-button");
+const tasksList = document.getElementById("tasks-list");
 
+const createTask = () => {
+  const taskText = taskInput.value;
+  const newTask = document.createElement("li");
+  newTask.textContent = taskText;
+  tasksList.appendChild(newTask);
+
+  taskInput.value = "";
+};
+
+const checkTask = (evt) => {
+  if (evt.target.tagName === "LI") {
+    evt.target.classList.toggle("active");
+  }
+};
+
+addButton.addEventListener("click", createTask);
+tasksList.addEventListener("click", checkTask);
